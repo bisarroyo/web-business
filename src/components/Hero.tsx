0407389076f40ce81b080c380a980799c61772a1
Bismark Arroyo <bis.arroyo@gmail.com>
2025-05-29 18:03:01 -0600

    create basic config with tailwind

---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div className='hero'>
+      <h1 className='hero-title'>Welcome to Our Site</h1>
+      <p className='hero-subtitle'>Experience the beauty of our services</p>
+    </div>
+  )
+}
